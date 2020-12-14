9cf09118777962d0250448c65e337c7c4b3592fe
qiang <guoshengqiang@live.com>
2020-12-14 11:42:53 +0800

    fix: 修复隐藏状态 margin 仍然显示的问题

--- weex-v-show.js
+++ weex-v-show.js
@@ -1,7 +1,18 @@
+const hiddenStyle = {
+  boxSizing: 'border-box',
+  marginTop: '0',
+  marginRight: '0',
+  marginBottom: '0',
+  marginLeft: '0',
+  width: '0',
+  height: '0',
+  overflow: 'hidden'
+}
+
 let oldSetStyle
 
 function setStyle (key, value, silent) {
-  if (this.__hidden__ && key === 'height' || key === 'overflow') {
+  if (this.__hidden__ && key in hiddenStyle) {
     this.__style__[key] = value
     return
   }
@@ -11,14 +22,12 @@ let oldSetStyles
 
 function setStyles (batchedStyles, silent) {
   if (this.__hidden__ && batchedStyles) {
-    if ('height' in batchedStyles) {
-      this.__style__.height = batchedStyles.height
-      delete batchedStyles.height
-    }
-    if ('overflow' in batchedStyles) {
-      this.__style__.overflow = batchedStyles.overflow
-      delete batchedStyles.overflow
-    }
+    Object.keys(hiddenStyle).forEach(key => {
+      if (key in batchedStyles) {
+        this.__style__[key] = batchedStyles[key]
+        delete batchedStyles[key]
+      }
+    })
   }
   oldSetStyles.call(this, batchedStyles, silent)
 }
@@ -27,10 +36,7 @@ let oldToStyle
 function toStyle () {
   const style = oldToStyle.call(this)
   if (this.__hidden__) {
-    Object.assign(style, {
-      height: '0',
-      overflow: 'hidden'
-    })
+    Object.assign(style, hiddenStyle)
   }
   return style
 }
@@ -40,11 +46,6 @@ function update (el, {
 }) {
   if (!el.__initVShow__) {
     el.__initVShow__ = true
-    const style = el.toStyle()
-    const realStyle = el.__realStyle__ = el.__realStyle__ || {}
-    realStyle.height = style.height || ''
-    realStyle.overflow = style.overflow || ''
-
     oldSetStyle = el.setStyle
     el.setStyle = setStyle
     oldSetStyles = el.setStyles
@@ -58,12 +59,14 @@ function update (el, {
   }
   el.__hidden__ = hidden
   if (hidden) {
-    oldSetStyles.call(el, {
-      height: '0',
-      overflow: 'hidden'
+    const oldStyle = oldToStyle.call(el)
+    const realStyle = el.__style__ = el.__style__ || {}
+    Object.keys(hiddenStyle).forEach(key => {
+      realStyle[key] = oldStyle[key] || ''
     })
+    oldSetStyles.call(el, hiddenStyle)
   } else {
-    oldSetStyles.call(el, el.__realStyle__)
+    oldSetStyles.call(el, el.__style__)
   }
 }
 
